a0adaf01d83b5b0b1076507e3cbd3797354f7dd1
Muzaffer Erdoğan <muzaffer45e@outlook.com>
2022-02-23 19:10:54 +0300

    weeather app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <current-data></current-data>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CurrentData from './components/CurrentData.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CurrentData
   }
 }
 </script>
---
+++ src/components/CurrentData.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="currentValue">Get Weather</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    currentValue() {
+      this.$http.get("api.openweathermap.org/data/2.5/weather?q=London,uk&appid=40fca5fb681271be23e8dbc689d5a4ad")
+        .then(
+          (response) => {
+            console.log(response.body);
+          },
+          (response) => {
+            console.log(response);
+          }
+        );
+    },
+  },
+};
+</script>
+
+<style>
+</style>
